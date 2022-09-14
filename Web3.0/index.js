445c162282e06e14a35bb8fb59f4bd2e53ba92c1
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-09-14 22:12:42 +0200

    Create index.js

---
+++ Web3.0/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('apqw'));
+root.render(
+  <h1>Moe pervoe soobsheniye v React JS</h1>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
