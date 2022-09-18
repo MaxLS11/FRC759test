0f8b098716b2d38b3bfef210947d489af7ca0c22
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-09-18 11:39:54 +0200

    Update App.js

--- Web3.0/App.js
+++ Web3.0/App.js
@@ -1,1 +1,15 @@
 
+import React, { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+
+  const [value, setValue] = useState("text");
+  return (
+    <div className="App">
+     <Counter/>
+    </div>
+  );
+}
+export default App;
