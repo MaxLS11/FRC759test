9abe38da0237cd1f342b484af420e6d3ef0c6f5c
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-09-18 11:37:31 +0200

    Update index.js

--- Web3.0/index.js
+++ Web3.0/index.js
@@ -5,7 +5,6 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('apqw'));
 root.render(
-  <h1>Moe pervoe soobsheniye v React JS</h1>,
   <React.StrictMode>
     <App />
   </React.StrictMode>
